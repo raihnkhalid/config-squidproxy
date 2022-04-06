196fad5e9f7b42c38fd60dd8eacdf91607ea946d
M Raihan Khalid <raihnkhalid@gmail.com>
2022-04-07 05:36:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
-#! /bin/bash
-sudo apt update
-sudo apt install squid -y
-sudo apt install zip -y
-sudo apt install apache2 -y
-sudo cd /etc/squid/
-sudo rm squid.conf
-sudo wget -O config.zip https://github.com/raihnkhalid/config-squidproxy/raw/main/config.zip
-sudo unzip config.zip
-sudo systemctl restart squid
-sudo systemctl enable squid
-echo -e "Installing Squid Proxy Server \e[1;32mSuccess! \e[0m";
+  #! /bin/bash
+  sudo apt update
+  sudo apt install squid -y
+  sudo apt install zip -y
+  sudo apt install apache2 -y
+  sudo rm /etc/squid/squid.conf
+  sudo wget -O /etc/squid/config.zip https://github.com/raihnkhalid/config-squidproxy/raw/main/config.zip
+  sudo unzip /etc/squid/config.zip -d /etc/squid/
+  sudo systemctl restart squid
+  sudo systemctl enable squid
+  echo -e "Installing Squid Proxy Server \e[1;32mSuccess! \e[0m";
