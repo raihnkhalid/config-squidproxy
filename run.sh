fb3f9779da216fc7787964999f4046bdd6ca656a
M Raihan Khalid <raihnkhalid@gmail.com>
2022-04-07 05:22:48 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ sudo apt install squid -y
 sudo apt install zip -y
 sudo apt install apache2 -y
 sudo cd /etc/squid/
+sudo rm squid.conf
 sudo wget -O config.zip https://github.com/raihnkhalid/config-squidproxy/raw/main/config.zip
 sudo unzip config.zip
 sudo systemctl restart squid
